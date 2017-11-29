a13364add1ef1f53ebc64798165e2963f30282e8
Braytiner <braytiner@hotmail.com>
2017-11-29 16:21:12 -0200

    Vídeo 1

---
+++ Auth/Auth.Api/AuthAuthorizationServerProvider.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Threading;
+
+namespace Auth.Api
+{
+    public class AuthAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            //Verifica se o token existe no cache criado pelo o OAuth e se o mesmo é válido 
+            //Se o token não for válido irá cair no segundo método
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Acces-Control-Allow-Origin", new[] { "*" });
+
+            try
+            {
+                var usuario = context.UserName;
+                var senha = context.Password;
+
+                //Aqui deveria ser feita a validação verificando o nome do usuário e senha no banco 
+                //de dados
+                if ((usuario != "braytiner") || (senha != "1234"))
+                {
+                    context.SetError("invalid_grant", "Usuário ou senha inválidos!");
+                    return;
+                }
+
+                var identidade = new ClaimsIdentity(context.Options.AuthenticationType);
+                identidade.AddClaim(new Claim(ClaimTypes.Name, usuario));
+
+                var roles = new List<string>();
+                //roles.Add("Admin");
+                roles.Add("User");
+
+                foreach (var role in roles)
+                    identidade.AddClaim(new Claim(ClaimTypes.Role, role));
+
+                //Setando usuário principal desta thread
+                GenericPrincipal principal = new GenericPrincipal(identidade, roles.ToArray());
+                Thread.CurrentPrincipal = principal;
+
+                context.Validated(identidade);
+            }
+            catch (System.Exception)
+            {
+
+                context.SetError("invalid_grant", "Falha ao autenticar");
+            }
+        }
+    }
+}
---
+++ Auth/Auth.Api/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Auth.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureWebApi(config);
+            ConfigureOAuth(app);
+
+            //Configurando CORS
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new Microsoft.Owin.PathString("/api/security/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(2),
+                Provider = new AuthAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
